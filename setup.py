dc467c674796260c399fd17ee090b0ab7107bf82
Joseph Deferio <j.deferio@gmail.com>
2021-01-27 10:47:23 -0500

    replace contact email address

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='healthdbETL',
     url='https://github.com/jdeferio/healthdbETL',
     author='Joseph Deferio',
-    author_email='josephjdeferio@gmail.com',
+    author_email='jdeferio.io@gmail.com',
     packages=['healthdbetl'],
     install_requires=[ 
         'sqlalchemy',
